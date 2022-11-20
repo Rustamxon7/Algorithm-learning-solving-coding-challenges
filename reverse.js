4f7905ce6787d7e21fec1e920d9b5bcba14950a2
Rustamjon Tolipov <rustamxontolipov788@gmail.com>
2022-11-20 15:46:13 +0500

    Reverse string Andrei's solution

--- reverse.js
+++ reverse.js
@@ -18,3 +18,32 @@ function reverse(str) {
 
   return getItems(array);
 }
+
+//////////////////////////////////
+// Solution by Andrei 🤓
+
+function reverse1(str) {
+  //check input
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'hmm that is not good';
+  }
+
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+  }
+  console.log(backwards);
+
+  return backwards.join('');
+}
+
+function reverse2(str) {
+  return str.split('').reverse().join('');
+}
+
+const reverse3 = (str) => str.split('').reverse().join('');
+
+const reverse4 = (str) => [...str].reverse().join('');
+
+console.log(reverse3('Hi My name is Andrei'));
